ce34c473538bc612a3123c83f97e9b6f24c73dd1
sagoheud <142952262+sagoheud@users.noreply.github.com>
2025-11-17 16:48:11 +0900

    Add files via upload

---
+++ 251117/IOT/source/20251117/bmi.py
@@ -0,0 +1,12 @@
+'''
+weight = float(input("몸무게를 kg 단위로 입력하시오.:"))
+height= float(input("키를 미터 단위로 입력하시오.:"))
+bmi = weight / height**2
+print(f"당신의 BMI = {bmi}")
+'''
+
+
+radius = float(input("반지름을 입력하시오:"))
+pi = 3.14159
+sphere = (4.0/3.0)*pi*radius**3
+print(f"구의 부피 = {sphere}")
---
+++ 251117/IOT/source/20251117/dividerange.py
@@ -0,0 +1,11 @@
+#a = 1
+#b = a/0
+#print("b")
+
+a = 1
+a += 1
+print(a)
+
+a <<= 2  # a = a << 1
+print(a)
+
