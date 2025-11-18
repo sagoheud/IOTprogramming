f6c790e2f765b1032c6cd5d66971e91349e768e4
sagoheud <142952262+sagoheud@users.noreply.github.com>
2025-11-18 14:40:29 +0900

    Add files via upload

---
+++ 251117/20251117/CircleArea.py
@@ -0,0 +1,3 @@
+radius = 10
+area = 3.14159 * radius * radius
+print("반지름이",radius,"인 원의 면적 =",area)
---
+++ 251117/20251117/constant.py
@@ -0,0 +1,9 @@
+a = 1
+b = 2
+print(a,b)
+t = a
+a = b
+b = t
+print(a,b)
+a, b = b, a
+print(a,b)
