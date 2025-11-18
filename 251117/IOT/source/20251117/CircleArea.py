b6b540ee8ac370dcf47f74ac95ea140765c3a0d8
sagoheud <142952262+sagoheud@users.noreply.github.com>
2025-11-18 14:40:13 +0900

    Delete 251117/IOT/source/20251117 directory

--- 251117/IOT/source/20251117/CircleArea.py
+++
@@ -1,3 +0,0 @@
-radius = 10
-area = 3.14159 * radius * radius
-print("반지름이",radius,"인 원의 면적 =",area)
--- 251117/IOT/source/20251117/constant.py
+++
@@ -1,9 +0,0 @@
-a = 1
-b = 2
-print(a,b)
-t = a
-a = b
-b = t
-print(a,b)
-a, b = b, a
-print(a,b)
